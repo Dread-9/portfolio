4a16f51d5687f664a7a3a789a4d6d07bd780b360
Dread <x56dragon@gmail.com>
2024-12-01 20:25:33 -0300

    add(page):Se añaden los componentes para el  portfolio y se configuran sus rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { AboutComponent } from './page/about/about.component';
+import { ProjectsComponent } from './page/projects/projects.component';
+import { ContactComponent } from './page/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contact', component: ContactComponent }
+];

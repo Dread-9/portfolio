5ea5ef5d59f6685661facd4ad9fa74f4ace20025
Dread <x56dragon@gmail.com>
2024-12-01 21:47:34 -0300

    feat(add):Se agrega footer y nuevos diseños de componentes

--- src/app/page/navbar/navbar.component.ts
+++ src/app/page/navbar/navbar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  ngOnInit(): void {
+    const hamburgerMenu = document.getElementById('hamburgerMenu');
+    const mobileMenu = document.getElementById('mobileMenu');
 
+    // Si los elementos existen, agrega el event listener
+    if (hamburgerMenu && mobileMenu) {
+      hamburgerMenu.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden'); 
+      });
+    }
+  }
 }
